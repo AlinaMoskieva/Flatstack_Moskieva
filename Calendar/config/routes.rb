b33ce0931cc301f7e87117c23b32adfb7b713e50
Alina Moskieva <alinamoskieva@gmail.com>
2016-02-06 21:49:50 +0300

    events to calendar added

--- Calendar/config/routes.rb
+++ Calendar/config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
   # You can have the root of your site routed with "root"
   root to: 'meetings#index'
+   get '/', to: 'meetings#index'
+   get '/meetings', to: 'meetings#index'
    get '/user/sign_in', to: 'devise/sessions#new'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
